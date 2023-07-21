678fbda51ec4f7223247a599cacfb06f6ff711fa
bihut <angelrzafra@gmail.com>
2023-07-21 09:39:08 +0200

    m

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import regex
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
@@ -68,12 +69,10 @@ def obtenerUsuario(nombre):
 
 def limpiarJSON(text):
     #text = 'Entendido, José. Aquí te dejo una opción de desayuno de aproximadamente 658 calorías que puedes consumir para ayudarte a bajar de peso:```json{  "desayuno": [    {      "alimento": "Huevos revueltos con espinacas",      "porcion": "2 huevos",      "calorias": 200    },    {      "alimento": "Pan integral tostado con aguacate",      "porcion": "2 rebanadas de pan",      "calorias": 458    }  ]}```Este desayuno incluye huevos revueltos con espinacas, que son ricos en proteínas y fibra, y pan integral tostado con aguacate, que proporciona gras'
-    regex = "\```(.*?)\```"
-    result = re.findall(regex, text)
-    result = result[0]
-    result = result.replace("json", "")
-    result = result.replace("['", "").replace("']", "")
-    data = json.loads(result)
+
+    pattern = regex.compile(r'\{(?:[^{}]|(?R))*\}')
+    result=pattern.findall(text)
+    data = json.loads(result[0])
     '''if "desayuno" in data:
         return data['desayuno']
     if "comida" in data:
